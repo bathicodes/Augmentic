2cb1cff24e27c5aeed8d6425ee78001d882fe198
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2021-08-16 10:36:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-from filters import rotation as rt
+from filters import rotation as rotation
 
 path = 'images/'
 
 dirpath = Path(path)
 
 for file in dirpath.iterdir():
-    rt.rotation(file, path)
+    rotation.rotation(file, path)
