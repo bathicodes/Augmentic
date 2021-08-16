82efc253d3e1cd2b64f6b95fa2c326b425181e00
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2021-08-16 10:32:13 +0530

    Updated rotation file
    added several angles

--- filters/rotation.py
+++ filters/rotation.py
@@ -1,9 +1,11 @@
 from PIL import Image
 from datetime import datetime
 
+rotation_angles = [10,20,30,40,50,60,70,80,90,100]
 
 def rotation(file, path):
-    with Image.open(file) as im:
-        name = str(datetime.now())+ ".jpg"
-        image = im.rotate(35)
-        image.save(path + name)
+    for angles in rotation_angles:
+        with Image.open(file) as im:
+            name = str(datetime.now())+ ".jpg"
+            image = im.rotate(angles)
+            image.save(path + name)
