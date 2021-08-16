31868e33c25b3005dc202d9ac1a5b8baa9d0d282
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2021-08-16 09:30:07 +0530

    Update main.py
    Added image saving feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from PIL import Image
+from datetime import datetime
 
 path = 'images'
 
@@ -8,5 +9,6 @@ dirpath = Path(path)
 
 for files in dirpath.iterdir():
     with Image.open(files) as im:
-        results = im.rotate(35)
-        results.show()
+        name = str(datetime.now())+ ".jpg"
+        image = im.rotate(35)
+        image.save("images/" + name)
