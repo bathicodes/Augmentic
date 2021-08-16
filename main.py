887937be43b6ce75981c46f92853c712a450b846
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2021-08-16 09:56:52 +0530

    Update main.py
    change the path of the folder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from PIL import Image
 from datetime import datetime
 
-path = 'images'
+path = '/Users/bathiyaseneviratne/Desktop/images/'
 
 dirpath = Path(path)
 
@@ -10,4 +10,4 @@ for files in dirpath.iterdir():
     with Image.open(files) as im:
         name = str(datetime.now())+ ".jpg"
         image = im.rotate(35)
-        image.save("images/" + name)
+        image.save(path + name)
