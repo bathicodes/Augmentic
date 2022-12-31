b7d3d9d01f7bc79e304e17be001c927d00968d36
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2022-12-31 07:38:00 +0530

    Update rotation.py
    Added white background generation for the rotation service.

--- filters/rotation.py
+++ filters/rotation.py
@@ -3,9 +3,23 @@ from datetime import datetime
 
 rotation_angles = [10,20,30,40,50,60,70,80,90,100]
 
+## Uncomment this part if you want black background 
+
+# def rotation(file, path):
+#     for angles in rotation_angles:
+#         with Image.open(file) as im:
+#             name = str(datetime.now())+ ".jpg"
+#             image = im.rotate(angles)
+#             image.save(path + name)
+
+ ##  this part provides you want white background 
+           
 def rotation(file, path):
     for angles in rotation_angles:
         with Image.open(file) as im:
             name = str(datetime.now())+ ".jpg"
-            image = im.rotate(angles)
-            image.save(path + name)
+            im2 = im.convert('RGBA')
+            rot = im2.rotate(angles, expand=1)
+            fff = Image.new('RGBA', rot.size, (255,)*4)
+            out = Image.composite(rot, fff, rot)
+            out.convert(im.mode).save(path + name)
